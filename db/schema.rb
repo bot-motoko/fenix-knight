d5ed7b0399e3be40fa6991ba50a7a0e639f3883a
sanemat <o.gata.ken@gmail.com>
2013-06-10 21:36:32 +0900

    Add provider

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130610124015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "providers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "providers", ["name"], name: "index_providers_on_name", unique: true, using: :btree
 
 end
