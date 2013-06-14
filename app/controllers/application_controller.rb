e3527d22f0afecb8f98273f99211571e79d2808e
Kenichi Murahashi <kenichi.murahashi@mrtaddy.com>
2013-06-14 16:05:02 +0900

    Use private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate
-  def authenticate
-    if Rails.env.production?
-      authenticate_or_request_with_http_basic('Enter Password') do |u, p|
-        u == ENV['USER_TAGGABLE'] && p == ENV['PASSWORD_TAGGABLE']
+
+  private
+    def authenticate
+      if Rails.env.production?
+        authenticate_or_request_with_http_basic('Enter Password') do |u, p|
+          u == ENV['USER_TAGGABLE'] && p == ENV['PASSWORD_TAGGABLE']
+        end
       end
     end
-  end
 end
